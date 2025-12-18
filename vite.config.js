27b1a3e151080ed22cbf8450946607a1d4eb6fa3
fairy <mac@kingdeMacBook-Air.local>
2025-12-19 00:09:06 +0800

    final

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 恢复最纯净的默认配置
 export default defineConfig({
   plugins: [react()],
-  // 不要加 base: './'，这会影响本地路由
-  // 不要加 outDir: 'build'，Vite 默认就是 dist
+  // 保持绝对干净，不要任何 extra 配置
 })
-
